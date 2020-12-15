6fe773d3f4a4f3815f6d31e98e5ad729dc450a96
odm275 <pomejia@gmail.com>
2020-12-15 16:48:11 -0600

    add global styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,33 @@ import { AuthProvider } from '@/lib/auth';
 import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import theme from '@/styles/theme';
 
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <AuthProvider>
-        <CSSReset />
+        <GlobalStyle />
         <Component {...pageProps} />
       </AuthProvider>
     </ChakraProvider>
